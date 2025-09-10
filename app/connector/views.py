9efd3e5a9902fc04f9835781088395a7e87269de
Ular <eldarakashka@gmail.com>
2025-09-10 11:14:34 +0600

    connect

--- app/connector/views.py
+++ app/connector/views.py
@@ -5,23 +5,6 @@ from ninja import Router, Query
 router = Router()
 
 
-# Создаем SSL-контекст из PFX
-client_key = "connector/secrets/client.key"
-client_crt = "connector/secrets/client.crt"
-ca_crt = "connector/secrets/ca.crt"
-url = '192.168.0.107'
-
-@router.get('/test-connector', response={200: str, 400: str})
-def test_connector(request):
-    # Подключаемся
-    response = requests.get(
-        url,
-        cert=(client_crt, client_key),  # клиентский сертификат + ключ
-        verify=ca_crt  # корневой сертификат (или True, если публичный CA)
-    )
-    return 200, response.text()
-
-
-@router.get('/get-info')
+@router.get('/get-info', response={200: str, 400: str})
 def get_list(request):
-    pass
+    return 200, 'good connect'
