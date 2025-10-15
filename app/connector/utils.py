7f533c5d6f0117e099d9694d681443d68075f6e0
Ular <eldarakashka@gmail.com>
2025-10-15 11:22:17 +0600

    add pagination

--- app/connector/utils.py
+++ app/connector/utils.py
@@ -1,39 +1,52 @@
 from jose import jwt
 
-def build_sql(group_cfg, subject: dict) -> str:
-    """
-    group_cfg = {
-        "from": {
-            "schema": "car",
-            "join": [
-                {"schema": "user", "on": "car.user_id = user.user_id"}
-            ],
-            "where_any": {
-                "car_id": "$.subject.car_id"
-            }
-        },
-        "select": {
-            "car_id": "car.car_id",
-            "title": "car.title",
-            "color": "car.color",
-            "full_name": "user.full_name",
-            "user_age": "user.age"
-        }
-    }
-    """
+
+def sql_select_only(group_cfg):
     from_cfg = group_cfg["from"]
-    schema = from_cfg["schema"]    # название таблицы
+    schema = from_cfg["schema"]  # название таблицы
 
     # SELECT car.car_id AS car_id, user.name AS user_name ...
     select_map = from_cfg["select"]
     cols = ", ".join([f"{src} AS {alias}" for alias, src in select_map.items()])
 
-    sql = f"SELECT {cols} FROM {schema}"
+    sql_only_select = f"SELECT {cols} FROM {schema}"
+    return sql_only_select
+
+
+def build_sql(group_cfg, subject: dict) -> str:
+    sql_only_select = sql_select_only(group_cfg)
+    from_cfg = group_cfg["from"]
 
     # JOIN
     if "join" in from_cfg:
         for j in from_cfg["join"]:
-            sql += f" JOIN {j['schema']} ON {j['on']}"
+            sql_only_select += f" JOIN {j['schema']} ON {j['on']}"
+
+    # WHERE
+    conditions = []
+    for field, path in from_cfg["where_any"].items():
+        if field in subject:
+            val = subject[field]
+            if isinstance(val, str):
+                val = f"'{val}'"
+            conditions.append(f"{field} = {val}")
+
+    if not conditions:
+        sql_only_select += " LIMIT 1"
+        return sql_only_select
+
+    sql = sql_only_select + " WHERE " + " AND ".join(conditions)
+    return sql
+
+
+def build_sql_like(group_cfg, subject: dict) -> str:
+    sql_only_select = sql_select_only(group_cfg)
+    from_cfg = group_cfg['from']
+
+    # JOIN
+    if "join" in from_cfg:
+        for j in from_cfg["join"]:
+            sql_only_select += f" JOIN {j['schema']} ON {j['on']}"
 
     # WHERE
     conditions = []
@@ -42,10 +55,8 @@ def build_sql(group_cfg, subject: dict) -> str:
             if field in subject:
                 val = subject[field]
                 if isinstance(val, str):
-                    val = f"'{val}'"
-                conditions.append(f"{field} = {val}")
+                    val = f"{val}"
+                conditions.append(f"{field} LIKE '%{val}%'")
 
-    if conditions:
-        sql += " WHERE " + " AND ".join(conditions)
-
+    sql = sql_only_select + " WHERE " + " AND ".join(conditions)
     return sql
--- app/connector/views.py
+++ app/connector/views.py
@@ -1,12 +1,13 @@
 from ninja import Router, Body
 import duckdb, hashlib, time, json, yaml, logging
 from pathlib import Path
-from .utils import build_sql
+from .utils import build_sql, build_sql_like
 from ninja_jwt.authentication import JWTAuth
 from jose import jwt
 import os
 from project.settings_local import SNAPSHOT_PATH, SECRETS_PATH
 import datetime
+import base64
 
 
 router = Router()
@@ -35,6 +36,9 @@ def get_db():
 
 @router.get("/v1/check-hash", response={200: str, 400: str}, auth=JWTAuth())
 def check_hash(request):
+    """
+    Сверяет хэши файлов баз parquet с указанными в manifest
+    """
     # проверка подписи
     schemas = CFG["schemas"]
     manifest_file = STORAGE_ROOT / CFG["storage"].get("manifest", "manifest.json")
@@ -56,13 +60,13 @@ def check_hash(request):
             logger.warning(f"Failed to read manifest.json: {e}")
 
 
-@router.post("/v1/lookup", auth=JWTAuth())
-def lookup(request, payload: dict = Body(...)):
+@router.post("/v1/lookup-like", response={200: dict, 400: str}, auth=JWTAuth())
+def lookup_like(request, payload: dict = Body(...)):
     """
     Пример тела запроса
     {
       "source_id": "CARS",
-      "subject": { "car_id": 1 },
+      "subject": { "gov_plate": "01KG517AUF" },
       "requested_fields": ["vehicles"]
     }
     """
@@ -75,37 +79,175 @@ def lookup(request, payload: dict = Body(...)):
     data = {}
     con = get_db()
 
+    group_name = requested_groups[0]
+    group_cfg = CFG.get("groups", {}).get(group_name)  # выбираем нужную группу из "groups" по названию "group_name"
+    if not group_cfg:
+        logger.info(f'Такой группы в mapping.yml нет: {group_name}')
+        return 400, f'Такой группы в mapping.yml нет: {group_name}'
+
+    sql = build_sql_like(group_cfg, subject)  # готовая sql команда для базы
+
+    ### адаптируем sql для обращения к parquet файлу ###
+    sql_parquet = sql_convert_parquet(group_cfg, sql)
+
+    ### обращаемся к parquet файлу и получаем данные ###
+    group_data = con.execute(sql_parquet).fetch_arrow_table().to_pylist()
+    for row in group_data:
+        for k, v in row.items():
+            if isinstance(v, (datetime.date, datetime.datetime)):
+                row[k] = v.isoformat()
+
+    data[group_name] = group_data
+
+    latency = int((time.time() - start) * 1000)
+    response = {
+        "source_id": source_id,
+        "status": "ok",
+        "source_status": "live",
+        "latency_ms": latency,
+        "data": data
+    }
+
+    response = jwt_encode_service(response)
+    return 200, response
+
+
+@router.post("/v1/lookup", auth=JWTAuth())
+def lookup(request, payload: dict = Body(...)):
+    """
+    Пример тела запроса:
+    {
+      "source_id": "CARS",
+      "subject": { "gov_plate": "01KG517AUF" },
+      "requested_fields": ["vehicles"],
+      "page": 1,
+      "page_size": 500
+    }
+    """
+
+    source_id = payload.get("source_id", "CARS")
+    subject = {k: v for k, v in payload.get("subject", {}).items() if bool(v)}
+    requested_groups = payload.get("requested_fields", []) or []
+
+    page = int(payload.get("page", 1))
+    page_size = int(payload.get("page_size", 500))
+    offset = (page - 1) * page_size
+
+    start = time.time()
+    data = {}
+    con = get_db()
+
     for group_name in requested_groups:
-        group_cfg = (CFG.get("groups") or {}).get(group_name)   # выбираем нужную группу из "groups" по названию "group_name"
-        schemas = CFG.get("schemas")
+        group_cfg = CFG.get("groups", {}).get(group_name)
         if not group_cfg:
             logger.info(f'Такой группы в mapping.yml нет: {group_name}')
             continue
 
         sql = build_sql(group_cfg, subject)
-        schema = group_cfg['from']
-        schema_name = schema['schema']
-        join_schema = schema.get('join')
+        sql_parquet = sql_convert_parquet(group_cfg, sql)
 
-        # schema_name
-        schema_cfg = schemas.get(schema_name)
-        parquet_file = schema_cfg.get('path')
+        # Подсчёт общего количества строк
+        count_sql = f"SELECT COUNT(*) AS total FROM ({sql_parquet})"
+        total_rows = con.execute(count_sql).fetchone()[0]
+
+        # Добавляем пагинацию
+        paginated_sql = f"{sql_parquet} LIMIT {page_size} OFFSET {offset}"
+        # Получаем только текущую страницу
+        result = con.execute(paginated_sql).fetch_arrow_table()
+        group_data = []
+        for batch in result.to_batches():
+            for row in batch.to_pylist():
+                for k, v in row.items():
+                    if isinstance(v, (datetime.date, datetime.datetime)):
+                        row[k] = v.isoformat()
+                    elif k in ('photo', 'signature') and isinstance(v, (bytes, bytearray)):
+                        row[k] = base64.b64encode(v).decode("utf-8")
+                group_data.append(row)
+
+        # Добавляем метаданные о пагинации
+        data[group_name] = {
+            "total": total_rows,
+            "page": page,
+            "page_size": page_size,
+            "has_next": offset + page_size < total_rows,
+            "results": group_data
+        }
+
+    latency = int((time.time() - start) * 1000)
+    response = {
+        "source_id": source_id,
+        "status": "ok",
+        "source_status": "live",
+        "latency_ms": latency,
+        "data": data
+    }
+
+    return jwt_encode_service(response)
+
+
+def sql_convert_parquet(group_cfg, sql) -> str:
+    """
+    Получает голый sql запрос,
+    Возвращает запрос к файлам 'parquet' на основе путей из group_cfg
+    """
+    schema = group_cfg['from']
+    schema_name = schema['schema']
+    join_schema = schema.get('join')
+
+    # schema_name
+    schema_cfg = CFG.get("schemas").get(schema_name)
+    parquet_file = schema_cfg.get('path')
+    parquet_path = (STORAGE_ROOT / parquet_file).resolve()
+
+    sql = sql.replace(f"FROM {schema_name}", f"FROM read_parquet('{parquet_path}') AS {schema_name}")
+    # если есть JOIN в группе group_cfg
+    if join_schema:
+        join_schema_name = join_schema[0].get("schema")
+        schema_cfg = CFG.get("schemas").get(join_schema_name)
+        parquet_file = schema_cfg.get("path")
         parquet_path = (STORAGE_ROOT / parquet_file).resolve()
-
-        sql = sql.replace(f"FROM {schema_name}", f"FROM read_parquet('{parquet_path}') AS {schema_name}")
-        # если есть JOIN в группе group_cfg
-        if join_schema:
-            join_schema_name = join_schema[0].get("schema")
-            schema_cfg = schemas.get(join_schema_name)
-            parquet_file = schema_cfg.get("path")
-            parquet_path = (STORAGE_ROOT / parquet_file).resolve()
+        if join_schema_name == 'document_images':
+            sql = sql.replace(f"JOIN {join_schema_name}", f"JOIN '{parquet_path}/*.parquet' AS {join_schema_name}")
+        else:
             sql = sql.replace(f"JOIN {join_schema_name}", f"JOIN read_parquet('{parquet_path}') AS {join_schema_name}")
 
-        group_data = con.execute(sql).fetch_arrow_table().to_pylist()
+    return sql
+
+
+@router.post('/get-photo', response={200: dict, 400: str}, auth=JWTAuth())
+def get_images(request, payload: dict = Body(...)):  # 100009209574
+    source_id = payload.get("source_id", "CARS")
+    subject = payload.get("subject", {})
+    requested_groups = payload.get("requested_fields", []) or []
+
+    start = time.time()
+    data = {}
+    con = get_db()
+
+    if 'document_images' in requested_groups:
+
+        group_name = requested_groups[0]
+        group_cfg = CFG.get("groups", {}).get(group_name)  # выбираем нужную группу из "groups" по названию "group_name"
+        if not group_cfg:
+            logger.info(f'Такой группы в mapping.yml нет: {group_name}')
+            return 400, f'Такой группы в mapping.yml нет: {group_name}'
+
+        sql = build_sql(group_cfg, subject)  # готовая sql команда для базы
+
+        ### адаптируем sql для обращения к parquet файлу ###
+        schema_name = group_cfg['from']['schema']
+        schema_cfg = CFG.get("schemas").get(schema_name)
+        parquet_file = schema_cfg['path']
+        parquet_path = (STORAGE_ROOT / parquet_file).resolve()
+        sql_parquet = sql.replace(f"FROM {schema_name}", f"FROM '{parquet_path}/*.parquet' AS {schema_name}")
+
+        ### обращаемся к parquet файлу и получаем данные ###
+        group_data = con.execute(sql_parquet).fetch_arrow_table().to_pylist()
         for row in group_data:
             for k, v in row.items():
-                if isinstance(v, (datetime.date, datetime.datetime)):
-                    row[k] = v.isoformat()
+                if k in ('photo', 'signature'):
+                    encoded_photo = base64.b64encode(v).decode("utf-8")
+                    row[k] = encoded_photo
 
         data[group_name] = group_data
 
@@ -118,17 +260,25 @@ def lookup(request, payload: dict = Body(...)):
         "data": data
     }
 
-    private_pem_path = f"{SECRETS_PATH}/private.pem"
-    if os.path.exists(private_pem_path):
-        with open(private_pem_path, "rb") as f:
-            private_pem = f.read()
-            jws_token = jwt.encode(response, private_pem, algorithm="RS256")
-        return {"jwt": jws_token}
+    response = jwt_encode_service(response)
 
     return response
 
 
-# --- API endpoints ---
-@router.get('/get-info', response={200: str, 400: str})
-def get_list(request):
-    return 200, 'good connect'
+def jwt_encode_service(body):
+    """
+    преобразует тело в jwt строку
+    """
+
+    if not SECRETS_PATH:
+        return body
+
+    private_pem_path = f"{SECRETS_PATH}/private.pem"
+    if os.path.exists(private_pem_path):
+        with open(private_pem_path, "rb") as f:
+            private_pem = f.read()
+            jws_token = jwt.encode(body, private_pem, algorithm="RS256")
+        return {"jwt": jws_token}
+
+    return body
+
